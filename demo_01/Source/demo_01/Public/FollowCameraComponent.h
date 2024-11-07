61cc73f4cc83bb938390f49ddcf1c6b3e8d47cc6
JulioJerez <jerezjulio0@gmail.com>
2024-11-07 14:41:39 -0800

    adding first demo

---
+++ demo_01/Source/demo_01/Public/FollowCameraComponent.h
@@ -0,0 +1,24 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Camera/CameraComponent.h"
+#include "FollowCameraComponent.generated.h"
+
+/**
+ * 
+ */
+UCLASS(ClassGroup = NewtonDemo, BlueprintType, Blueprintable, meta=(BlueprintSpawnableComponent), HideCategories = (Physics, Collision))
+class DEMO_01_API UFollowCameraComponent : public UCameraComponent
+{
+	GENERATED_BODY()
+	
+	virtual void PostLoad() override;
+	virtual void BeginPlay() override;
+	virtual void EndPlay(EEndPlayReason::Type Reason) override;
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+
+	FTransform m_localTransform;
+	bool m_beginPlay = false;
+};
